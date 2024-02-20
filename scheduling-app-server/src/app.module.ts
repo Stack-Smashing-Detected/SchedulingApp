4671f9d15d57d4be6042788ea835de3bc692f706
VicJo <VicJohnathanWilson@outlook.com>
2024-02-20 13:34:44 +0800

    created api route for users, includes users.module, users.service, users.create.dto and users.controller

--- scheduling-app-server/src/app.module.ts
+++ scheduling-app-server/src/app.module.ts
@@ -1,10 +1,22 @@
-import { Module } from '@nestjs/common';
-import { AppController } from './app.controller';
-import { AppService } from './app.service';
+import { Module } from '@nestjs/common'
+import { ConfigModule, ConfigService} from '@nestjs/config';
+import { MongooseModule} from '@nestjs/mongoose';
+import { UsersModule } from './users/users.module';
+
 
 @Module({
-  imports: [],
-  controllers: [AppController],
-  providers: [AppService],
+  imports: [
+    ConfigModule.forRoot({ isGlobal: true}),
+    MongooseModule.forRootAsync({
+    // load connection string from .env file. 
+    imports: [ConfigModule],
+    inject: [ConfigService],
+    useFactory: async (config: ConfigService) => ({
+      uri: config.get<string>('MONGODB_URI'), 
+    })
+  }), UsersModule],
+  controllers: [],
+  providers: [],
 })
-export class AppModule {}
+
+export class AppModule{}
--- scheduling-app-server/src/main.ts
+++ scheduling-app-server/src/main.ts
@@ -1,8 +1,14 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import * as dotenv from 'dotenv';
 
+console.log(process.env.MONGODB_CONNECTION_STRING);
 async function bootstrap() {
+  dotenv.config();
   const app = await NestFactory.create(AppModule);
   await app.listen(3000);
 }
+
+
+
 bootstrap();
